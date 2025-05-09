7fe00c0f9b38886e86cf13f0e4c5de850fc3dd70
Rmin <armin890ebp@gmail.com>
2025-05-09 19:18:34 +0330

    secondQ

--- 2024/2/main.go
+++ 2024/2/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -13,7 +14,7 @@ func readInput() []string {
 	}
 	//fmt.Println(data)
 	var sb strings.Builder
-	length := len(data)
+	length := len(data) - 1
 	for i := 0; i < length; i++ {
 		if data[i] <= '9' && '0' <= data[i] {
 			sb.WriteByte(data[i])
@@ -28,26 +29,54 @@ func readInput() []string {
 
 	return strings.Split(s, ",")
 }
-func safe_increasing(slice []string) bool {}
+func safe_increasing(slice []string) bool {
 
-func safe_decreasing(slice []string) bool {}
+	for i := 1; i <= len(slice)-1; i++ {
+		a, _ := strconv.Atoi(slice[i])
+		b, _ := strconv.Atoi(slice[i-1])
+		if a <= b {
+
+			return false
+		} else if (a - b) > 3 {
+			return false
+		}
+
+	}
+	return true
+}
+
+func safe_decreasing(slice []string) bool {
+
+	for i := 1; i <= len(slice)-1; i++ {
+		a, _ := strconv.Atoi(slice[i])
+		b, _ := strconv.Atoi(slice[i-1])
+		if a >= b {
+			return false
+		} else if (b - a) > 3 {
+			return false
+		}
+	}
+
+	return true
+}
 
 func secondQ() int {
 	data := readInput()
 	var count int = 0
 	for _, x := range data {
-		//	fmt.Println(x)
 		row := strings.Split(x, " ")
-		if safe_increasing(row) || safe_decreasing(row) {
+
+		if safe_decreasing(row) {
+			count += 1
+		} else if safe_increasing(row) {
 			count += 1
 		}
+		fmt.Println("\n\n\n\n")
 	}
-
 	return count
 }
 
 func main() {
 
-	secondQ()
-
+	fmt.Println(secondQ())
 }
